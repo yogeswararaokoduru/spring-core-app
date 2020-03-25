ad2abfdc3c38c1d6c30ff2249ffd037fd7c7565c
yogeswararaokoduru <yogikkoduru@gmail.com>
2020-03-25 12:29:24 +0530

    spring core code
    simple spring core applicarion

---
+++ src/main/java/com/org/spring/core/SpringCoreApp.java
@@ -0,0 +1,13 @@
+package com.org.spring.core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringCoreApp {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+        obj.getMessage();
+    }
+}
